db024cd3c7087aded6931aa06ac6e110a6a1cc61
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-10-21 13:08:30 +0200

    initialising core apps

--- tms/urls.py
+++ tms/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vehicles/', include('vehicles.urls')),
+    path('routes/', include('routes.urls')),
+    path('schedules/', include('schedules.urls')),
+    path('tracking/', include('tracking.urls')),
 ]
