690012460e2c89c2a6133d32c9cec282f863e881
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-10-22 22:51:03 +0200

    base template layouts

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 def main_view(request):
-    return render(request, 'homepage.html')
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
--- tms/settings.py
+++ tms/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,7 +46,7 @@ ROOT_URLCONF = 'tms.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'base/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,6 +108,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'base/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
