1f0ba83ae823e5c63bf0f8362e2e77dcf980405c
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-10-24 14:09:51 +0200

    setup auth

--- tms/settings.py
+++ tms/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'core',
     'vehicles',
     'drivers',
-    'routes'
+    'routes',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = "login"
+LOGOUT_REDIRECT_URL = "home"
--- tms/urls.py
+++ tms/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('vehicles/', include('vehicles.urls')),
     path('drivers/', include('drivers.urls')),
     path('routes/', include('routes.urls'))
