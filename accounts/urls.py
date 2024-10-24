958830c31ead2d87d35657a1c4256ad504c94fa1
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-10-24 15:19:03 +0200

    setup custom auth views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_register
+from .views import user_register, user_logout, user_login
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
--- accounts/views.py
+++ accounts/views.py
@@ -11,3 +11,21 @@ def user_register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+#custom login methods
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'registration/login.html', {'error': 'Invalid username or password.'})
+    return render(request, 'registration/login.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("home")
