b5ce5f2c9db870fa8b73eb560c82377958818d14
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-11-14 11:32:30 +0200

    refactor: removed core app

--- tms/urls.py
+++ tms/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='homepage.html'), name='homepage'),
     path('accounts/', include('accounts.urls')),
     path('vehicles/', include('vehicles.urls')),
     path('drivers/', include('drivers.urls')),
